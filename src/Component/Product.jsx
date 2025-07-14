3293c7edd461ab2e5b1ffb7f661971a261a783e1
Virendra Sah <rajvirendra8875@gmail.com>
2025-07-14 13:02:09 +0530

    initial commit

--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react'
-function Product({ category , search}) {
+function Product({ category, search }) {
     const [products, setProducts] = useState([])
     const fetchProducts = async () => {
         const response = await fetch('https://fakestoreapi.com/products')
         const data = await response.json()
         setProducts(data)
     }
-    const filterProduct = products.filter(product =>{
-        const matchsCategory= (category ? product.category===category : true)
-        const matchesSearch= (product.title.toLowerCase().includes(search.toLowerCase()))
+    const filterProduct = products.filter(product => {
+        const matchsCategory = (category ? product.category === category : true)
+        const matchesSearch = (product.title.toLowerCase().includes(search.toLowerCase()))
         return matchsCategory && matchesSearch
     })
     useEffect(() => {
@@ -17,12 +17,16 @@ function Product({ category , search}) {
     return (
         <div>
             <h1 className="text-3xl font-bold text-center">Products</h1>
-            <div className="grid grid-cols-3 gap-4">
-                {filterProduct.map((product, index) => (
-                    <ProductCard key={index} product={product} />
-                ))}
+                {filterProduct.length == 0
+                    ? <div className=' w-full text-2xl font-bold text-center mt-10'>No Product Found</div>
 
-            </div>
+                    :
+                    <div className="grid grid-cols-3 gap-4 pb-10">
+                        {filterProduct.map((product, index) => (
+                            <ProductCard key={index} product={product} />
+                        ))}
+                    </div>
+                }
         </div>
     )
 }
@@ -32,7 +36,9 @@ export { Product }
 function ProductCard({ product }) {
     return (
         <div className="border-2 border-gray-300 rounded-md p-4 text-start">
-            <img src={product.image} alt={product.title} className="w-full h-48 object-cover" />
+            <figure className='h-48 flex items-center justify-center'>
+            <img src={product.image} alt={product.title} className="h-48 object-cover flex items-center justify-center" />
+            </figure>
             <h2 className='truncate'><span className='font-bold'>Product Name: </span>{product.title}</h2>
             <p><span className='font-bold'>Category: </span>{product.category}</p>
             <p><span className='font-bold'>Price: </span>{product.price}</p>
