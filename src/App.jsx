d3529f4d84c7c35122814f643702df1c1805d9bc
Virendra Sah <rajvirendra8875@gmail.com>
2025-07-14 11:48:40 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import { Product } from './Component/product'
 function App() {
-  let [category, usecategory]=useState('All')
+  let [category, usecategory] = useState('')
 
-  const onsubmit=(e)=>{
+  const onsubmit = (e) => {
     usecategory(e.target.value)
   }
   return (
@@ -11,18 +11,22 @@ function App() {
     <div className='grid grid-cols-[15%_85%] px-5 mt-5 gap-3'>
       <div className='flex flex-col gap-5 '>
         <h3 className='text-2xl font-bold text-center'>filter</h3>
-        <div>
-          <select onChange={onsubmit} name="categoryfilter"  id="" className='w-full p-2 rounded-md border-2 border-gray-300'>
-            <option value="all">All</option>
+        <div className='flex flex-col gap-3'>
+          <div>
+            <label htmlFor="" className='font-bold'>Search</label>
+            <input type="text" className='border w-full rounded p-2' />
+          </div>
+          <select onChange={onsubmit} name="categoryfilter" id="" className='w-full p-2 rounded-md border-2 border-gray-300'>
+            <option value="">All</option>
             <option value="electronics">Electronics</option>
             <option value="jewelery">Jewelery</option>
-            <option value="men's clothing">Men's Clothing</option>
+            <option value="men's clothing">men's clothing</option>
           </select>
         </div>
       </div>
       <div className='w-full'>
-        <Product category={category}/>
-      </div> 
+        <Product category={category} />
+      </div>
     </div>
 
   )
--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -1,36 +1,36 @@
 import { useState, useEffect } from 'react'
-function Product ({category}){
-    console.log(category)
+function Product({ category }) {
     const [products, setProducts] = useState([])
-    const [filterproduct, setFilterproduct]= useState()
     const fetchProducts = async () => {
         const response = await fetch('https://fakestoreapi.com/products')
         const data = await response.json()
         setProducts(data)
     }
-    // const (products.filter((item)=> item.category==category) 
+    const filterProduct = products.filter(product => {
+        return (category ? product.category===category : true)})
+    console.log(filterProduct)
     useEffect(() => {
         fetchProducts()
     }, [category])
-    return(
+    return (
         <div>
             <h1 className="text-3xl font-bold text-center">Products</h1>
             <div className="grid grid-cols-3 gap-4">
-                {products.map((product, index) => (
-                        <ProductCard key={index} product={product}/>
-                    ))}
-                
+                {filterProduct.map((product, index) => (
+                    <ProductCard key={index} product={product} />
+                ))}
+
             </div>
         </div>
     )
 }
 
-export {Product}
+export { Product }
 
-function ProductCard({product}){
-    return(
+function ProductCard({ product }) {
+    return (
         <div className="border-2 border-gray-300 rounded-md p-4 text-start">
-            <img src={product.image} alt={product.title} className="w-full h-48 object-cover"/>
+            <img src={product.image} alt={product.title} className="w-full h-48 object-cover" />
             <h2 className='truncate'><span className='font-bold'>Product Name: </span>{product.title}</h2>
             <p><span className='font-bold'>Category: </span>{product.category}</p>
             <p><span className='font-bold'>Price: </span>{product.price}</p>
